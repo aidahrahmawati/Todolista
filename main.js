a2ecb1f7e48d6af9a6e83354b0979a8b30154971
mhilham <mhilham16@gmail.com>
2025-01-22 20:35:28 +0700

    memunculkan tugas hari senin

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
-console.log('Hello World!');
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCksetmQe_ec2BH6g5MKqQU_1K1U6htmww",
+  authDomain: "data-7d32f.firebaseapp.com",
+  projectId: "data-7d32f",
+  storageBucket: "data-7d32f.appspot.com",
+  messagingSenderId: "156748846014",
+  appId: "1:156748846014:web:4269883b14bdb400b2dfef",
+  measurementId: "G-W3SBB85TF1"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambildaftarsenin() {
+  const refDokumen = collection(db, "senin");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikankueri = await getDocs(kueri);
+  
+  let hasil = [];
+  cuplikankueri.forEach((dok) => {
+    hasil.push({ 
+      id: dok.id, 
+      tugas: dok.data().tugas,
+      status:dok.data().status,
+      prioritas: dok.data().prioritas,
+      tanggal:dok.data().tanggal,
+      });
+  });
+  
+  return hasil;
+}
